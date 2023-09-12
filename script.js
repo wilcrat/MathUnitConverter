3043ecfdc3b114b22052130c3eb0d95845f249de
Wilsonmobisa <willexjumah12338@gmail.com>
2023-09-12 21:21:56 +0300

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+function convertLength() {
+  const inputValue = parseFloat(document.getElementById("inputValue").value);
+  const unitFrom = document.getElementById("unitFrom").value;
+  const unitTo = document.getElementById("unitTo").value;
+  let result;
+
+  if (unitFrom == "meters" && unitTo == "feet") {
+    result = inputValue * 3.28084;
+  } else result = inputValue;
+}
+
+document.getElementById("result").textContent = result;
